b231662718a2970d36a9e0caad0f7b84083dc52f
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-12 14:24:13 -0700

    rough draft, maybe final?

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # About Me
 Assignment for Code 201 to create a personal page with a guessing game.
 
-I like popsicles.
+I like popsicles. And dogs. And bacon. And I can/will eat Mexican food every day.
--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert('HEY Y\'ALL');
+alert('Hi! Let\'s play a guessing game about me!');
+
+var prompt1 = prompt('is james allergic to cats? y/n').toUpperCase();
+if (prompt1 === 'YES' || prompt1 === 'Y') {
+  alert('yeah it really sucks');
+} else {
+  alert('nah you\'re wrong');
+}
+
+var prompt2 = prompt('does james believe in aliens? y/n').toUpperCase();
+if (prompt2 === 'YES' || prompt2 === 'Y') {
+  alert('not really, but probability might say...');
+} else {
+  alert('wait til independence day and you\'ll start to believe');
+}
+
+var prompt3 = prompt('is james color blind? y/n').toUpperCase();
+if (prompt3 === 'YES' || prompt3 === 'Y') {
+  alert('yeah, that \'brown\' page yesterday was definitely black');
+} else {
+  alert('i sure hope not');
+}
+
+var prompt4 = prompt('finally, would james answer an unknown question with a yes or no?').toUpperCase();
+if (prompt4 === 'YES' || prompt4 === 'Y') {
+  alert('i guess you guessed right');
+} else {
+  alert('i guess you guessed no');
+}
