3896939bb3f96aeb179de585d852110d773e1e02
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-15 09:19:42 -0700

    syntax corrections. removed console.log statements used for debugging

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 alert('Hi! Let\'s play a guessing game about me!');
 
 var countRight = 0;
+
 var questions = ['does james snowboard? y/n', 'has james played an instrument? y/n', 'is james good at golf? y/n', 'can james eat mexican food all day? y/n', 'has james only lived in washington? y/n', 'I\'m thinking of a number between 1-10. What is it?', 'Where\'s a city in Washington I have lived in?'];
+
 var correctAns = ['y', 'y', 'n', 'y', 'n', 5, ['seattle', 'renton', 'bellevue']];
+
 var corrResponses = ['yep sure do!', 'right. he played the violin for 13 years.', 'geez no faith. but you\'re kinda right.', 'sure can!', 'you\'re right! i\'ve also lived in oregon', 'you guessed right', 'congrats. you\'re right. here are all the possibilities: ' + correctAns[6]];
+
 var wrongResponses = ['nah you\'re wrong', 'wrong answer', 'thanks, but you\'re wrong', 'challenge accepted', 'you\'re wrong!', ['you guessed too low. try again', 'you guessed too high. try again', 'you didn\'t choose a number. try again'], 'okay you\'re done. here were the possible answers: ' + correctAns[6]];
 
 questionYN = function(q) {
@@ -47,12 +51,9 @@ questionCities = function(q) {
   var cityChances = 6;
   var wrong = true;
   while (wrong === true) {
-    console.log('while loop ran');
     for (var i = 0; i < cityChances; i++) {
-      console.log('outside for loop ran');
       var answer = prompt(questions[q]).toLowerCase();
       for (var j = 0; j < correctAns[q].length; j++) {
-        console.log('inside for loop ran');
         if (answer == correctAns[q][j]) {
           alert(corrResponses[q]);
           wrong = false;
